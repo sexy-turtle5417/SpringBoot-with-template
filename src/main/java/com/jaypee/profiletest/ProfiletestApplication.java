0a4f2c506e34dd89664a8b37627ce0740d89f8dd
Jaypee <jaypee.zulieta@lsu.edu.ph>
2023-05-13 22:35:37 +0800

    initial commit

---
+++ src/main/java/com/jaypee/profiletest/ProfiletestApplication.java
@@ -0,0 +1,13 @@
+package com.jaypee.profiletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfiletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfiletestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaypee/profiletest/ProfiletestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jaypee.profiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class ProfiletestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
